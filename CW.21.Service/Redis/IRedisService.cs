7df538d73c328b958ce4e3ea2720b5c70fbc41e2
Sina Farahmandian <sinafarahmandian101@gmail.com>
2026-06-26 16:32:53 +0330

    uncompleted 27A

---
+++ CW.21.Service/Redis/IRedisService.cs
@@ -0,0 +1,9 @@
+namespace CW._21.Services.Redis;
+
+public interface IRedisService
+{
+    Task<T?> GetAsync<T>(string key);
+    Task SetAsync<T>(string key, T value, TimeSpan? expiry = null);
+    Task RemoveAsync(string key);
+    Task<bool> ExistsAsync(string key);
+}
